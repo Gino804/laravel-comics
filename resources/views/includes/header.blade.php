0bbea2ccc32ecfd3ce8fdf19d0114c159c54b70c
Gino804 <giacomo.p03@gmail.com>
2023-08-03 15:32:12 +0200

    Added header

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,21 @@
+<header>
+  <div class="container">
+    <nav>
+      <figure>
+        <img src="{{ asset('images/dc-logo.png') }}" alt="DC Logo">
+      </figure>
+      <ul>
+        <li><a href="#">CHARACTERS</a></li>
+        <li><a href="#" class="active">COMICS</a></li>
+        <li><a href="#">MOVIES</a></li>
+        <li><a href="#">TV</a></li>
+        <li><a href="#">GAMES</a></li>
+        <li><a href="#">COLLECTIBLES</a></li>
+        <li><a href="#">VIDEOS</a></li>
+        <li><a href="#">FANS</a></li>
+        <li><a href="#">NEWS</a></li>
+        <li><a href="#">SHOP</a></li>
+      </ul>
+    </nav>
+  </div>
+</header>
