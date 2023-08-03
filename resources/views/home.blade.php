51b31683edd3010a42626ac55a2ffab606dec6ca
Gino804 <giacomo.p03@gmail.com>
2023-08-03 16:20:48 +0200

    Added main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,27 @@
 @extends('layouts.main')
+
+@section('main-content')
+  <main>
+    <section id="jumbotron">
+      <img src="{{ asset('images/jumbotron.jpg') }}" alt="Jumbotron">
+    </section>
+    <section id="comics">
+      <div class="container">
+        <legend>CURRENT SERIES</legend>
+        <div class="row">
+          @foreach ($comics as $comic)
+            <div class="column">
+              <div class="card">
+                <figure>
+                  <img src="{{ $comic['thumb'] }}">
+                </figure>
+                <h5>{{ $comic['series'] }}</h5>
+              </div>
+            </div>
+          @endforeach
+        </div>
+      </div>
+      <a href="#" class="button">LOAD MORE</a>
+    </section>
+  </main>
+@endsection
